bdc456816981123bf8655492ef232f8c549ca47b
Debbie30 <cherotichdebby11@gmail.com>
2024-05-28 02:40:52 +0300

    initial commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        def are_anagrams(word1, word2):
+            return sorted(word1) == sorted(word2)
+        
+        matches = [anagram for anagram in possible_anagrams if are_anagrams(self.word, anagram)]
+        return matches
 # your code goes here!
